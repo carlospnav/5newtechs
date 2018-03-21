6b80936fb31d960c38ad8ec697e60b2cc3cb0b28
Carlos P Navarrete Filho <karnav@gmail.com>
2018-03-21 11:12:59 -0300

    Add gradient to background.

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -2,23 +2,30 @@ import { graphql } from "react-apollo"
 import { branch, compose, withProps, renderComponent } from "recompose"
 import { fetchPolicyList } from "lib/constants"
 import peopleQuery from "queries/people"
+import styled from 'styled-components';
 import map from "lodash/map"
 import Link from "common/Link"
 import Spinner from "common/Spinner"
-import MainContainer from "./common/MainContainer";
-import Background from "./common/background/Background";
+import ListContainer from '../components/common/ListContainer';
+
+const List = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+`
 
 const PeopleList = ({ items }) => {
   return (
-    <ul>
-      {map(items, item => (
-        <li key={item.id}>
-          <Link route="person_detail" personId={item.id}>
-            <button>{item.name}</button>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <ListContainer>
+      <List>
+        {map(items, item => (
+          <li key={item.id}>
+            <Link route="person_detail" personId={item.id}>
+              <button>{item.name}</button>
+            </Link>
+          </li>
+        ))}
+      </List>
+    </ListContainer>
   )
 }
 
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -8,4 +8,5 @@ export default styled.header.attrs({
   height: ${props => props.main ? '800px' : '300px' }
   width: 100%;
   z-index: 1;
+  background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5));
 `
