e1f9151b4378f88e5f7659b35092cf3581b5b2c4
Carlos P Navarrete Filho <karnav@gmail.com>
2018-03-27 09:48:56 -0300

    Redirect to people from index page.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { compose } from "recompose"
-import withData from "lib/withData"
-import withSetUrlParams from "lib/withSetUrlParams"
-import PeopleListContainer from "components/PeopleListContainer"
-import App from "components/App"
-import MainContainer from '../src/components/common/MainContainer'
-import Background from '../src/components/common/background/Background'
-import Header from '../src/components/common/header/Header'
-import PageLogo from '../src/components/common/pageLogo/PageLogo'
+import React from "react"
 
-function IndexPage({ url: { pathname } }) {
-  return (
-    <App>
-      <Background />
-      <Header main>
-        <PageLogo />
-      </Header>
-      <MainContainer>
-        <PeopleListContainer />
-      </MainContainer>
-    </App>
-  )
+export default class Index extends React.Component {
+  static getInitialProps = ({res}) => {
+    res.redirect("/people")
+  }
 }
-
-export default compose(withData, withSetUrlParams)(IndexPage)
