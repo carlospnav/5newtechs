c7450e48806b74ac37327dd7577f88a18f864292
Carlos P Navarrete Filho <karnav@gmail.com>
2018-03-28 09:06:01 -0300

    Revert Avatar component to wrapper, commented..

--- src/components/common/avatar/Avatar.js
+++ src/components/common/avatar/Avatar.js
@@ -1,26 +1,10 @@
-// import styled from 'styled-components';
-// import { avatarProps } from 'theme/utils';
-
-// /* Avatar Component - Renders a small or large
-//   avatar image based on the small prop passed to it.
-// */
-// let componentProps;
-
-// export default styled.img.attrs({
-//   initProps: props => {
-//     componentProps = avatarProps(props.small)
-//   },
-//   src: props => componentProps.src,
-//   width: props => props.small ? '140px' : '300px',
-//   height: props => props.small ? '132.6px' : '283px'
-// })`
-//   object-fit: contain;
-// `
-
-
 import styled from 'styled-components';
 import { avatarProps } from 'theme/utils';
 
+
+/* Avatar Component - Renders a small or large
+  avatar image based on the small prop passed to it.
+*/
 const Img = styled.img.attrs({
   width: props => props.small ? '140px' : '300px',
   height: props => props.small ? '132.6px' : '283px'
