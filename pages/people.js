0be39a1f062fb9514d441ebc78a34c5effa1d1a1
Carlos P Navarrete Filho <karnav@gmail.com>
2018-03-30 11:39:52 -0300

    Add styled components snapshot testing functionality.

--- pages/people.js
+++ pages/people.js
@@ -3,10 +3,10 @@ import withData from "lib/withData"
 import App from "components/App"
 import PeopleListContainer from "components/people/PeopleListContainer"
 import withSetUrlParams from "lib/withSetUrlParams"
-import MainContainer from "../src/components/common/MainContainer";
-import Background from "../src/components/common/background/Background";
-import PageLogo from "../src/components/common/pageLogo/PageLogo";
-import Header from '../src/components/common/header/Header';
+import MainContainer from "common/mainContainer/MainContainer";
+import Background from "common/background/Background";
+import PageLogo from "common/pageLogo/PageLogo";
+import Header from 'common/header/Header';
 
 function PeoplePage({ url: { pathname } }) {
   return (
